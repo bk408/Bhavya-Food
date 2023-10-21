2fcea3f996106712ca62fc72c4c0d43352e97a4c
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-10-21 21:36:50 +0530

    Header component created

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+import Header from "./src/components/Header";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<AppLayout />);
